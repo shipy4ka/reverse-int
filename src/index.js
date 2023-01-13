c29be97d8512365e3310a8d12dd56ddbec3d2eaa
shipy4ka <marinauglanica@gmail.com>
2023-01-13 22:23:06 +0300

    docs: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+ let result;
+ let nPositive;
+ if ( n < 0 ){
+    nPositive = 0 - n;
+    result = nPositive.toString().split('').reverse().join('');
+ } else {result = n.toString().split('').reverse().join('');}
+    
+    return Number(result);
 }
